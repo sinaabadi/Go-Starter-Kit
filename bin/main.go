7ae27ce546848caf6ac8b86bfcbfae762cdc49fa
Sina Abadi <sina_abadi@hotmail.com>
2019-09-09 18:12:13 +0430

    Complete config package. add gitignore

--- bin/main.go
+++ bin/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"starter-kit/config"
 )
 
-
 func main() {
 	config := config.GetConfig()
 	engine := gin.Default()
-
+	host := config.Get(`host`).(string)
+	port := config.Get(`port`).(string)
+	goEnv :=
+	engine.Run(fmt.Sprintf(`%v:%v`, host, port))
 
 }
--- config/config.go
+++ config/config.go
@@ -1,17 +1,39 @@
 package config
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"starter-kit/utils"
+)
+
 var config Config
+var envConfig map[string]interface{}
+var defaultConfig map[string]interface{}
 
 type Config struct {
-	keys map[string]interface{}
+	runEnv string
 }
 
-func (c *Config) get(key string) interface{} {
-
+func (c Config) Get(key string) interface{} {
+	envValue := os.Getenv(envConfig[key].(string))
+	if len(envValue) != 0 {
+		return envValue
+	}
+	switch c.runEnv {
+	default:
+		return defaultConfig[key]
+	}
 }
 
 func init() {
-
+	envTxt, _ := ioutil.ReadFile(`config/env.json`)
+	defaultTxt, _ := ioutil.ReadFile(`config/default.json`)
+	_ = json.Unmarshal(envTxt, &envConfig)
+	_ = json.Unmarshal(defaultTxt, &defaultConfig)
+	setConfig(Config{
+		runEnv: utils.GetEnv(`GO_ENV`, `development`),
+	})
 }
 
 func setConfig(c Config) {
