0d63c13e9cb69eedced7271aaffefe77659c01ed
Sina Abadi <sina_abadi@hotmail.com>
2020-12-18 17:42:27 +0330

    feat: add swagger

--- bin/main.go
+++ bin/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/gin-gonic/contrib/gzip"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"io"
 	"log"
 	"starter-kit/apps"
@@ -11,7 +13,51 @@ import (
 	"starter-kit/constants"
 	"starter-kit/middlewares"
 	"starter-kit/utils"
+
+	_ "github.com/swaggo/gin-swagger/example/basic/docs"
 )
+
+// @title Golang Starter kit API
+// @version 1.0
+// @description This is a sample server celler server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:3000
+// @BasePath /api/v1
+
+// @securityDefinitions.basic BasicAuth
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+// @securitydefinitions.oauth2.application OAuth2Application
+// @tokenUrl https://example.com/oauth/token
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.implicit OAuth2Implicit
+// @authorizationUrl https://example.com/oauth/authorize
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.password OAuth2Password
+// @tokenUrl https://example.com/oauth/token
+// @scope.read Grants read access
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.accessCode OAuth2AccessCode
+// @tokenUrl https://example.com/oauth/token
+// @authorizationUrl https://example.com/oauth/authorize
+// @scope.admin Grants read and write access to administrative information
 
 func main() {
 
@@ -33,6 +79,9 @@ func main() {
 	engine.Static(`/public`, `public`)
 	apps.RegisterApps(engine)
 	engine.NoRoute(middlewares.NoRouteHandler)
+	url := ginSwagger.URL("http://localhost:3000/swagger/doc.json")
+	engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+
 	err := engine.Run(fmt.Sprintf(`%v:%v`, host, port))
 	if err != nil {
 		log.Panicf(`Could not start server => %v`, err)
--- go.mod
+++ go.mod
@@ -1,23 +1,36 @@
 module starter-kit
 
-go 1.12
+go 1.15
 
 require (
-	github.com/appleboy/gin-jwt/v2 v2.6.2
-	github.com/gin-gonic/contrib v0.0.0-20190923054218-35076c1b2bea
-	github.com/gin-gonic/gin v1.4.0
-	github.com/go-resty/resty v0.0.0-00010101000000-000000000000
-	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/google/go-cmp v0.4.0 // indirect
-	github.com/nicksnyder/go-i18n/v2 v2.0.3
-	github.com/sirupsen/logrus v1.4.2
-	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/appleboy/gin-jwt/v2 v2.6.4
+	github.com/aws/aws-sdk-go v1.36.11 // indirect
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-openapi/spec v0.20.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-resty/resty v1.12.0
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/golang/snappy v0.0.2 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/klauspost/compress v1.11.3 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/nicksnyder/go-i18n/v2 v2.1.1
+	github.com/sirupsen/logrus v1.7.0
+	github.com/swaggo/gin-swagger v1.3.0 // indirect
+	github.com/swaggo/swag v1.7.0 // indirect
+	github.com/ugorji/go v1.2.1 // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
-	go.mongodb.org/mongo-driver v1.1.1
-	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/text v0.3.2
+	go.mongodb.org/mongo-driver v1.4.4
+	golang.org/x/crypto v0.0.0-20201217014255-9d1352758620 // indirect
+	golang.org/x/net v0.0.0-20201216054612-986b41b23924 // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+	golang.org/x/sys v0.0.0-20201218084310-7d0127a74742 // indirect
+	golang.org/x/text v0.3.4
+	golang.org/x/tools v0.0.0-20201218024724-ae774e9781d2 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/resty.v1 v1.12.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
 replace github.com/go-resty/resty => gopkg.in/resty.v1 v1.11.0
